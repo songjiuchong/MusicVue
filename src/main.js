4241def3b2651ec42a6c72d1b08a2e5e1fdd49de
jiusong <jiusong@ebay.com>
2018-06-19 21:13:33 +0800

    3-2

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
-// import router from './router'
+import fastclick from 'fastclick'
+import router from './router'
 
 import './common/stylus/index.styl'
 
 Vue.config.productionTip = false
 
+fastclick.attach(document.body)
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  // router,
+  router,
   render: h => h(App)
 })
